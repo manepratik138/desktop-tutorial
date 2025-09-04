b7ebd1e0aa2dc50923e1874534f3812f8e627db9
PRATIK MANE <pratikmane138@gmail.com>
2025-09-04 14:46:11 +0530

    Add files via upload

--- LOOP.py
+++ LOOP.py
@@ -1,69 +1,128 @@
-#Q1. Print numbers from 1 to 20 using a for loop.
-for i in range(1,21):
-    print(i)
-#Q2. Print the squares of numbers from 1 to 10.
+"""#1.Print numbers from 1 to 10 using a for loop.
 for i in range(1,11):
-    square=i*i
-    print("square:",square)
-#Q3. Print the multiplication table of 7.
-num=7
-for i in range(1,11):
-    table=num*i
-    print(table)
-#Q4. Print all even numbers between 1 and 50.
-for i in range(2,51,2):
+    print("numbers",i)
+    
+#2.Print numbers from 10 to 1 using a while loop.
+i=10
+while i>=1:
     print(i)
-"""#Q5. Print the sum of numbers from 1 to 100.
+    i-=1
+    
+#3.Print the multiplication table of a given number (e.g., 7).
+x=7
+i=1
+while i<=10:
+    print("multipication table:",x*i)
+    i+=1
+#using for loop
+n=7
+for i in range(1,11):
+    print(n*i)
+    
+#4.Find the sum of numbers from 1 to 100 using a loop.
 sum=0
 for i in range(1,101):
     sum+=i
-print("total sum:",sum)"""
-#Q6. Print each character of a string "Python".
-str="python"
-for ch in str:
-    print(ch)
-#Q7. Count how many vowels are in a string.
-text="india is my country"
-vowels="aeiouAEIOU"
-count=0
-for ch in text:
-    if ch in vowels:
-        count+=1
-print("vowels:",count)
-#Q8. Print each element of a list [10, 20, 30, 40, 50].
-list= [10, 20, 30, 40, 50]
-for el in list:
-    print(el)
-#adding the total number in list.
-total=0
-numbers=[12,3,34,5,2312,132,3,42,4]
-for el in numbers:
-    total+=el
-print("total addition:",total)
-print("total addition by other method:",sum(numbers))
+print(sum)
+#using while loop
+sum=0
+i=1
+while i<=100:
+    sum+=i
+    i+=1
+print("sum",sum)
+#5.Print all even numbers between 1 and 50.
+for i in range(2,51,2):
+    print("even no.",i)
+#while loop
+i=0
+while i<=50:
+    i+=2
+    print("even ",i)
+    
+#6.Print all odd numbers between 1 and 50.
+for i in range(1,51,2):
+    print("odd no.",i)
+i=1
+while i<=49:
+    i+=2
+    print("odd",i)
 
-##using for loop print thre star...................
-for i in range(5):
-    for j in range(5):
+#7.Print the factorial of a given number using a loop.
+def fact(n):
+    fact=1
+    for i in range(1,n+1):
+        fact*=i
+    return(fact)
+print(fact(5))
         
-       print("*",end="")
-    print("*")
-    
-    
-#3Print the cube of numbers from 1 to 10.
-for i in range(1,11):
-    cube=i*i*i
-    print("cube:",cube)
-    
-#Print the sum of all even numbers between 1 and 100.
-add=0
-for i in range(2,100,2):
-    add+=i
-print("addition:",add)
+#using while loop
 
-#Print the factorial of a number using a for loop.
-n=int(input("enter any number:"))
-fact=1
-for i in range(1,n+1):
-    fact*=i
-print("factorial:",fact)
+def num(n):
+    fact=1
+    i=1
+    while i<=n:
+        fact*=i
+        i+=1
+    return fact
+print(num(5))
+
+#check the word are present in string 
+s="india is my country"
+x="india"
+words=s.split()
+for word in words:
+    if word==x:
+        print("found",x)
+        break
+    
+    else:
+        print("not found",x)
+print(s.endswith("country"))
+
+##check the character are present in string 
+s="i love my india"
+ch="m"
+if ch in s:
+    print("found",ch)
+else:
+    print("not found")
+
+#check the charactes comes how many times
+s="india is my country"
+ch="i"
+count=0
+for c in s:
+    if c==ch:
+        count+=1
+print("total count:",count)"""
+    
+#Find the largest number in a list
+list=[32,43,43,1,3,6,5,76,43,11]
+max_num=list[0]
+for num in list:
+    if num>max_num:
+        max_num=num
+print("max number:",max_num)
+#find the smallest  umber in the list
+list=[54,73,82,92,923,923,88,99,65]
+min_num=list[0]
+for val in list:
+    if val<min_num:
+        min_num=val
+print("minimum value:",min_num)
+
+#count even odd numbers in list
+list=[12,23,43,54,54,32,21,11,45,665,6542,1134,95,]
+even_count=0
+odd_count=0
+for c in list:
+    if c%2==0:
+        even_count+=1
+        print(even_count)
+    else:
+        odd_count+=1
+        print(odd_count)
+        
+    
+        
